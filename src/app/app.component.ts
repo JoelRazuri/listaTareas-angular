889a386645b48c9613462e91b0e716d10e917e46
Joel Razuri <jrazuri98@hotmail.com>
2024-04-25 21:31:32 -0300

    ts component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TasksService } from './services/tasks.service';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule , FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'listaTareasApp';
+export class AppComponent implements OnInit{
+  listTasks: string[] = [];
+  newTask: string = '';
+
+  private _tasksService = inject(TasksService);
+
+
+  ngOnInit(): void {
+    this.listTasks = this._tasksService.getTasks();
+  }
+
+  addTask() {
+    this._tasksService.addTask(this.newTask);
+    this.newTask = '';
+    this.listTasks = this._tasksService.getTasks();
+  }
+
+  deleteTask(index: number) {
+    this._tasksService.deleteTask(index);
+    this.listTasks = this._tasksService.getTasks();
+  }
+
 }
